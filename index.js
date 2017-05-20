3b66a38ba455c354958031c5c24418e55ff8c94c
Chandima Ranaaweera <chan4lk@gmail.com>
2017-05-20 15:02:23 +0530

    bot builder added.

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-var http = require('http');
+var restify = require('restify');
+var builder = require('botbuilder');
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure!! from local");
-
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
-var port = process.env.PORT || 1337;
-server.listen(port);
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 
-console.log("Server running at http://localhost:%d", port);
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
+});
