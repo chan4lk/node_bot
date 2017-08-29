f0c818325c1f7121d5ad6fb4f6de817031850d21
Chandima Ranaweera <chan4lk@gmail.com>
2017-08-30 01:45:57 +0530

    common questions added.

--- faqs.js
+++ faqs.js
@@ -1,4 +1,40 @@
-const faqs = [];
+const commonFaqs = [{
+        questions: ['Hi', 'Hello', 'Good Morning', 'Good Afternoon'],
+        answers: ['Hi I\'m Vortex bot. What can I do for you', 'Hello, Nice to meet you']
+    },
+    {
+        questions: ['VortexBot', 'Who are you'],
+        answers: ['Welcome to Vortex Faq Bot.\n Ask questions from view faq section. Any other message will be echoed.']
+    },
+    {
+        questions: ['chandima'],
+        answers: ['Welcome Chandima, Hope you are doing well :)']
+    }
+];
+
+const getRandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+};
+
+const answerCommon = (question) => {
+    let foundAnswer = false;
+    let answer = '';
+    commonFaqs.forEach(function (faq) {
+        if (foundAnswer) return;
+        faq.questions.forEach(function (q) {
+            if (foundAnswer) return;
+            if (q.toLowerCase() === question.replace('?', '').toLowerCase()) {
+                answer = faq.answers[getRandomInt(0,faq.answers.length - 1)];
+                foundAnswer = true;
+                return;
+            }
+        }, this)
+    }, this);
+
+    return answer;
+};
 
 /**
  * Get answer for given question
@@ -9,18 +45,18 @@ const getAnswer = (question, faqs) => {
     let answer = 'you said ' + question;
     let foundAnswer = false;
     faqs.forEach((faq) => {
+        if (foundAnswer) return;
         if (faq.question.toLowerCase().indexOf(question.replace('?', '').toLowerCase()) > -1) {
             answer = faq.answer;
             foundAnswer = true;
             return;
         }
-        if (foundAnswer) return;
     });
 
     return answer;
 }
 
-module.exports = {
-    faqs,
+module.exports = {    
+    answerCommon,
     getAnswer
 };
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv-extended').load();
 var builder = require('botbuilder');
 var restify = require('restify');
 const {
+    answerCommon,
     getAnswer
 } = require('./faqs');
 
@@ -30,41 +31,45 @@ var instructions = 'Welcome to Vortex Faq Bot.\n' +
 
 var bot = new builder.UniversalBot(connector, function (session) {
     var reply = new builder.Message()
-    .address(session.message.address);
-
-    getFaqs().then(function (faqs) {
-        
-
-        var text = session.message.text.toLocaleLowerCase();
-        switch (text) {
-            case 'show me a hero card':
-                reply.text('Sample message with a HeroCard attachment')
-                    .addAttachment(new builder.HeroCard(session)
-                        .title('Sample Hero Card')
-                        .text('Displayed in the DirectLine client'));
-                break;
-
-            case 'send me a botframework image':
-                reply.text('Sample message with an Image attachment')
-                    .addAttachment({
-                        contentUrl: 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png',
-                        contentType: 'image/png',
-                        name: 'BotFrameworkOverview.png'
-                    });
-
-                break;
-
-            default:
-                reply.text(getAnswer(session.message.text, faqs));
-                break;
-        }
-
+        .address(session.message.address);
+    var text = session.message.text.toLocaleLowerCase();
+    const answer = answerCommon(text);
+    if (answer) {
+        reply.text(answer);
         session.send(reply);
-    }, function(error){
-        reply.text('I can\'t help you sorry');
-        session.send(reply)
-    });
+    } else {
+        getFaqs().then(function (faqs) {
+            switch (text) {
+                case 'show me a hero card':
+                    reply.text('Sample message with a HeroCard attachment')
+                        .addAttachment(new builder.HeroCard(session)
+                            .title('Sample Hero Card')
+                            .text('Displayed in the DirectLine client'));
+                    break;
 
+                case 'send me a botframework image':
+                    reply.text('Sample message with an Image attachment')
+                        .addAttachment({
+                            contentUrl: 'https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png',
+                            contentType: 'image/png',
+                            name: 'BotFrameworkOverview.png'
+                        });
+
+                    break;
+
+                default:
+                    reply.text(getAnswer(session.message.text, faqs));
+                    break;
+            }
+
+
+            session.send(reply);
+        }, function (error) {
+            reply.text('I can\'t help you sorry');
+            session.send(reply)
+        });
+
+    }
 
 
 });
